7b2b99f6a71afa7abd06be556bea410ebeeaafff
cux-maks <volcano020716@gmail.com>
2024-01-14 01:21:34 +0900

    add: SignInController 추가

---
+++ src/main/java/bcsd/backend/project/pokku/controller/SignInController.java
@@ -0,0 +1,22 @@
+package bcsd.backend.project.pokku.controller;
+
+import bcsd.backend.project.pokku.dto.SignInRequest;
+import bcsd.backend.project.pokku.dto.SignInResponse;
+import bcsd.backend.project.pokku.service.SignInServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/sign-in")
+public class SignInController {
+
+    private SignInServiceImpl signInService;
+    @PostMapping
+    public ResponseEntity<SignInResponse> signin(@RequestBody SignInRequest request) throws Exception{
+        return new ResponseEntity<>(signInService.login(request), HttpStatus.OK);
+    }
+}
