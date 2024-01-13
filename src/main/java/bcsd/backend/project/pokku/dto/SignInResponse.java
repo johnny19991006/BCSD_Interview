8bf2484c29bfb0fd2be136bbaa2df37caec6a6d6
cux-maks <volcano020716@gmail.com>
2024-01-14 00:55:55 +0900

    add: SignInResponse 추가

---
+++ src/main/java/bcsd/backend/project/pokku/dto/SignInResponse.java
@@ -0,0 +1,40 @@
+package bcsd.backend.project.pokku.dto;
+
+import bcsd.backend.project.pokku.domain.Authority;
+import bcsd.backend.project.pokku.domain.UserInfo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignInResponse {
+    private String userName;
+    private String userBirth;
+    private String userTel;
+    private String userEmail;
+    private String userId;
+    private String userPassword;
+    private String userNickname;
+    private String userEducation;
+    private String token;
+    private List<Authority> authorities = new ArrayList<>();
+
+    public SignInResponse(UserInfo user){
+        this.userName = user.getUserName();
+        this.userBirth = user.getUserBirth();
+        this.userTel = user.getUserTel();
+        this.userEmail = user.getUserEmail();
+        this.userId = user.getUserId();
+        this.userPassword = user.getUserPassword();
+        this.userNickname = user.getUserNickname();
+        this.userEducation = user.getUserEducation();
+        this.authorities = user.getAuthorities();
+    }
+}
