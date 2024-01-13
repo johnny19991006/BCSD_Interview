b41325c77d623c5d3eac3b57a334f067abdd44e4
cux-maks <volcano020716@gmail.com>
2024-01-14 02:29:01 +0900

    add: UserRequest 추가

---
+++ src/main/java/bcsd/backend/project/pokku/dto/UserRequest.java
@@ -0,0 +1,17 @@
+package bcsd.backend.project.pokku.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRequest {
+    private String userName;
+    private String userBirth;
+    private String userTel;
+    private String userEmail;
+    private String userId;
+    private String userPassword;
+    private String userNickname;
+    private String userEducation;
+}
