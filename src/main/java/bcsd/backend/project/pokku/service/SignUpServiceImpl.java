458755c942271e7d403549978d93ac88df52f8a9
cux-maks <volcano020716@gmail.com>
2024-01-14 19:59:10 +0900

    modified: Authority save 추가

--- src/main/java/bcsd/backend/project/pokku/service/SignUpServiceImpl.java
+++ src/main/java/bcsd/backend/project/pokku/service/SignUpServiceImpl.java
@@ -1,5 +1,6 @@
 package bcsd.backend.project.pokku.service;
 
+import bcsd.backend.project.pokku.dao.AuthorityRepository;
 import bcsd.backend.project.pokku.dao.UserInfoRepository;
 import bcsd.backend.project.pokku.domain.Authority;
 import bcsd.backend.project.pokku.domain.UserInfo;
@@ -19,6 +20,7 @@ public class SignUpServiceImpl implements SignUpService{
 
     private final UserInfoRepository userInfoRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthorityRepository authorityRepository;
 
     @Override
     public boolean register(SignUpRequest request) throws Exception{
@@ -33,12 +35,14 @@ public class SignUpServiceImpl implements SignUpService{
                     .userTel(request.getUserTel())
                     .userEducation(request.getUserEducation())
                     .build();
-
-            userInfo.setRoles(Collections.singletonList(Authority.builder()
+            Authority auth = Authority.builder()
                     .AuthName("ROLE_User")
-                    .build()));
+                    .build();
+            userInfo.setRoles(Collections.singletonList(auth));
 
             userInfoRepository.save(userInfo);
+            authorityRepository.save(auth);
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
             throw new Exception("잘못된 요청입니다.");
