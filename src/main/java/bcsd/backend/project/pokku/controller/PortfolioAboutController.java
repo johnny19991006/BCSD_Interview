7842d08d2aa187026c19e949284d5cb532754939
cux-maks <volcano020716@gmail.com>
2024-01-15 15:11:49 +0900

    add: find 추가

--- src/main/java/bcsd/backend/project/pokku/controller/PortfolioAboutController.java
+++ src/main/java/bcsd/backend/project/pokku/controller/PortfolioAboutController.java
@@ -1,16 +1,14 @@
 package bcsd.backend.project.pokku.controller;
 
 import bcsd.backend.project.pokku.dto.PortfolioAboutRequest;
+import bcsd.backend.project.pokku.dto.PortfolioAboutResponse;
 import bcsd.backend.project.pokku.dto.SignInRequest;
 import bcsd.backend.project.pokku.dto.SignInResponse;
 import bcsd.backend.project.pokku.service.PortfolioAboutServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/about")
@@ -22,8 +20,13 @@ public class PortfolioAboutController {
         this.portfolioAboutService = service;
     }
 
-    @PostMapping
+    @PutMapping
     public ResponseEntity<Boolean> updatePortfolioAbout(@RequestBody PortfolioAboutRequest request) throws Exception{
         return new ResponseEntity<>(portfolioAboutService.updatePortfolioAbout(request), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<PortfolioAboutResponse> findPortfolioAbout(@RequestBody PortfolioAboutRequest request) throws Exception{
+        return new ResponseEntity<>(portfolioAboutService.findPortfolioAbout(request), HttpStatus.OK);
     }
 }
