f3f0d799a307d4764ca8481f132c1ae93814d80d
cux-maks <volcano020716@gmail.com>
2024-01-15 14:54:23 +0900

    modified: userId 추가

--- src/main/java/bcsd/backend/project/pokku/dto/PortfolioAboutRequest.java
+++ src/main/java/bcsd/backend/project/pokku/dto/PortfolioAboutRequest.java
@@ -1,6 +1,7 @@
 package bcsd.backend.project.pokku.dto;
 
 public class PortfolioAboutRequest {
+    private String userId;
     private Boolean userNameVisible;
     private Boolean userTelVisible;
     private Boolean userEmailVisible;
