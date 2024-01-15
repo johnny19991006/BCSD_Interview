a45bc174e4e47801ecc6d49c8a1270394a78020c
cux-maks <volcano020716@gmail.com>
2024-01-15 14:06:21 +0900

    modified: UserDFRequest 추가, (Delete, Find)

---
+++ src/main/java/bcsd/backend/project/pokku/dto/UserDFRequest.java
@@ -0,0 +1,11 @@
+package bcsd.backend.project.pokku.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDFRequest {
+    private String userId;
+    private String token;
+}
