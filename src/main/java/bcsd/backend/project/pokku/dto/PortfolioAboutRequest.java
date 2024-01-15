7b270d8db43cc2c895c30e5e99823bc09e9360e1
cux-maks <volcano020716@gmail.com>
2024-01-15 14:57:35 +0900

    add: Getter, Setter 추가

--- src/main/java/bcsd/backend/project/pokku/dto/PortfolioAboutRequest.java
+++ src/main/java/bcsd/backend/project/pokku/dto/PortfolioAboutRequest.java
@@ -1,5 +1,10 @@
 package bcsd.backend.project.pokku.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PortfolioAboutRequest {
     private String userId;
     private Boolean userNameVisible;
