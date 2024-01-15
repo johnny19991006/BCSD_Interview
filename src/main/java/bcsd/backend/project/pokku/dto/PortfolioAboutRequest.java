55bbc360a0195c36743b2ef26fc36947bc2e9aad
cux-maks <volcano020716@gmail.com>
2024-01-15 14:52:16 +0900

    add: PortfolioAboutRequest 추가

---
+++ src/main/java/bcsd/backend/project/pokku/dto/PortfolioAboutRequest.java
@@ -0,0 +1,8 @@
+package bcsd.backend.project.pokku.dto;
+
+public class PortfolioAboutRequest {
+    private Boolean userNameVisible;
+    private Boolean userTelVisible;
+    private Boolean userEmailVisible;
+    private Boolean userEducationVisible;
+}
