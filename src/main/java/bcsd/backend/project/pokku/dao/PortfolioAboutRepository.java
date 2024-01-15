859e487a7718df1299e8871e314b608d6cf4f47c
cux-maks <volcano020716@gmail.com>
2024-01-15 15:11:34 +0900

    add: findbyUserId 추가

--- src/main/java/bcsd/backend/project/pokku/dao/PortfolioAboutRepository.java
+++ src/main/java/bcsd/backend/project/pokku/dao/PortfolioAboutRepository.java
@@ -2,8 +2,15 @@ package bcsd.backend.project.pokku.dao;
 
 import bcsd.backend.project.pokku.domain.PortfolioAbout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PortfolioAboutRepository extends JpaRepository<PortfolioAbout, Long> {
+
+    @Query("SELECT p FROM PortfolioAbout p WHERE p.userInfo = :user_id")
+    Optional<PortfolioAbout> findByUserId(@Param("user_id") String user_id);
 }
