7865587e0c188269b8e0c8890f8f44ae43de5770
cux-maks <volcano020716@gmail.com>
2024-01-15 15:48:22 +0900

    modified: findById, updateById 작성

--- src/main/java/bcsd/backend/project/pokku/dao/PortfolioAboutRepository.java
+++ src/main/java/bcsd/backend/project/pokku/dao/PortfolioAboutRepository.java
@@ -1,7 +1,10 @@
 package bcsd.backend.project.pokku.dao;
 
 import bcsd.backend.project.pokku.domain.PortfolioAbout;
+import bcsd.backend.project.pokku.domain.UserInfo;
+import bcsd.backend.project.pokku.dto.PortfolioAboutRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,5 +15,9 @@ import java.util.Optional;
 public interface PortfolioAboutRepository extends JpaRepository<PortfolioAbout, Long> {
 
     @Query("SELECT p FROM PortfolioAbout p WHERE p.userInfo = :user_id")
-    Optional<PortfolioAbout> findByUserId(@Param("user_id") String user_id);
+    Optional<PortfolioAbout> findByUserId(@Param("user_id") UserInfo user);
+
+    @Modifying
+    @Query("UPDATE PortfolioAbout p SET p.userEducationVisible = :EducationVisible, p.userEmailVisible = :EmailVisible, p.userNameVisible = :NameVisible, p.userTelVisible = :TelVisible WHERE p.userInfo = :user")
+    void updateById(@Param("user") UserInfo user, @Param("EducationVisible") Boolean education, @Param("EmailVisible") Boolean email, @Param("NameVisible") Boolean name, @Param("TelVisible") Boolean tel);
 }
