1885a9acf90c922a81cadbb481633b43234172c6
cux-maks <volcano020716@gmail.com>
2024-01-16 16:42:18 +0900

    add: findByUserId 추가

--- src/main/java/bcsd/backend/project/pokku/dao/UserInfoBlogRepository.java
+++ src/main/java/bcsd/backend/project/pokku/dao/UserInfoBlogRepository.java
@@ -1,9 +1,18 @@
 package bcsd.backend.project.pokku.dao;
 
+import bcsd.backend.project.pokku.domain.UserInfo;
 import bcsd.backend.project.pokku.domain.UserInfoBlog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserInfoBlogRepository extends JpaRepository<UserInfoBlog, String> {
+
+    @Query("SELECT p FROM UserInfoBlog p WHERE p.userInfo = :user_id")
+    Optional<UserInfoBlog> findByUserId(@Param("user_id") UserInfo user);
+
 }
--- src/main/java/bcsd/backend/project/pokku/dao/UserInfoInstagramRepository.java
+++ src/main/java/bcsd/backend/project/pokku/dao/UserInfoInstagramRepository.java
@@ -1,9 +1,18 @@
 package bcsd.backend.project.pokku.dao;
 
+import bcsd.backend.project.pokku.domain.UserInfo;
 import bcsd.backend.project.pokku.domain.UserInfoInstagram;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserInfoInstagramRepository extends JpaRepository<UserInfoInstagram, String> {
+
+    @Query("SELECT p FROM UserInfoInstagram p WHERE p.userInfo = :user_id")
+    Optional<UserInfoInstagram> findByUserId(@Param("user_id") UserInfo user);
+
 }
