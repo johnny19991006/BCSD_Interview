e9709e228aa3d14cdcef02ca3c04b02404fb6c56
cux-maks <volcano020716@gmail.com>
2024-01-16 16:43:47 +0900

    add: UserInfoSNSResponse 추가

---
+++ src/main/java/bcsd/backend/project/pokku/dto/UserInfoSNSResponse.java
@@ -0,0 +1,17 @@
+package bcsd.backend.project.pokku.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class UserInfoSNSResponse {
+    private String userId;
+    private String userGithub;
+    private String userBlog;
+    private String userInstagram;
+}
