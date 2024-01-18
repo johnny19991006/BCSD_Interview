342cae8fcc328d6645b7f05777cd506aeca2243a
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-01-18 22:25:10 +0900

    Post Service

---
+++ src/main/java/com/forum/forum_site/service/PostService.java
@@ -0,0 +1,17 @@
+package com.forum.forum_site.service;
+
+import com.forum.forum_site.dto.SavePostDto;
+import com.forum.forum_site.dto.UpdatePostDto;
+
+import javax.annotation.processing.FilerException;
+
+public interface PostService {
+    // 글 게시
+    void savePost(SavePostDto savePostDto) throws FilerException;
+
+    // 글 수정
+    void updatePost(Integer id, UpdatePostDto updatePostDto);
+
+    // 글 삭제
+    void deletePost(Integer id);
+}
---
+++ src/main/java/com/forum/forum_site/service/PostServiceImpl.java
@@ -0,0 +1,23 @@
+package com.forum.forum_site.service;
+
+import com.forum.forum_site.dto.SavePostDto;
+import com.forum.forum_site.dto.UpdatePostDto;
+
+import javax.annotation.processing.FilerException;
+
+public class PostServiceImpl implements PostService{
+    @Override
+    public void savePost(SavePostDto savePostDto) throws FilerException {
+
+    }
+
+    @Override
+    public void updatePost(Integer id, UpdatePostDto updatePostDto) {
+
+    }
+
+    @Override
+    public void deletePost(Integer id) {
+
+    }
+}
