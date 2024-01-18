3d4c92fb65f0be5688977037d6cfb45de7c40937
HyeonsuLee <leehyeonsu4888@naver.com>
2024-01-18 14:11:29 +0900

    feat: JwtTokenProvider구현

--- src/main/java/BCSD/MusicStream/controller/MusicController.java
+++ src/main/java/BCSD/MusicStream/controller/MusicController.java
@@ -3,6 +3,7 @@ package BCSD.MusicStream.controller;
 import BCSD.MusicStream.domain.Music;
 import BCSD.MusicStream.service.MusicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +17,8 @@ import java.util.List;
 public class MusicController {
     @Autowired
     private MusicService musicService;
-
     @GetMapping("/{musicName}")
     public ResponseEntity<List<Music>> getMusicByMusicName(@PathVariable String musicName) {
-        System.out.println(musicName);
         return ResponseEntity.ok(musicService.getMusicByMusicName(musicName));
     }
 }
--- src/main/java/BCSD/MusicStream/controller/UserController.java
+++ src/main/java/BCSD/MusicStream/controller/UserController.java
@@ -1,9 +1,20 @@
 package BCSD.MusicStream.controller;
 
+import BCSD.MusicStream.domain.Music;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequestMapping("/user")
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @GetMapping("/login")
+    public ResponseEntity<List<Music>> getAccesToken(@PathVariable String email, String password) {
+        return null;
+    }
+
 }
