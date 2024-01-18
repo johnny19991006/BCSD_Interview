ef1f20d31ade49b3196cf7b6176ee3e6831fa0b6
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-01-18 19:15:19 +0900

    domain - Post

--- src/main/java/com/forum/forum_site/domain/Post.java
+++ src/main/java/com/forum/forum_site/domain/Post.java
@@ -5,8 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-
 import static jakarta.persistence.FetchType.LAZY;
 
 @Table(name = "Post")
