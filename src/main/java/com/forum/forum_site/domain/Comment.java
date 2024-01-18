4827044b4b78378d315737c14cfc34ff127f0285
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-01-18 20:34:53 +0900

    fix comment entity

--- src/main/java/com/forum/forum_site/domain/Comment.java
+++ src/main/java/com/forum/forum_site/domain/Comment.java
@@ -1,22 +1,34 @@
 package com.forum.forum_site.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
 
 @Table(name = "Comment")
 @Data
 @NoArgsConstructor
 @Entity
 public class Comment {
+
     @Id @GeneratedValue
     private Integer comment_id;
 
+    @ManyToOne(fetch = LAZY)
     @Column(nullable = false)
-    private Integer post_id;
+    @JoinColumn(name = "post_id")
+    private Post post;
 
+    @ManyToOne(fetch = LAZY)
     @Column(nullable = false)
-    private Integer author_id;
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
 
     @Lob
     @Column(nullable = false)
@@ -24,4 +36,15 @@ public class Comment {
 
     @Column
     private Integer likes_count;
+
+    private boolean is_Removed= false;
+
+    @Builder
+    public Comment( User author, Post post, Comment parent, String content) {
+        this.author = author;
+        this.post = post;
+        this.parent = parent;
+        this.content = content;
+        this.is_Removed = false;
+    }
 }
