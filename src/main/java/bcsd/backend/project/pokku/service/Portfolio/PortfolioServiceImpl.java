66028dcf0071f074b8cc9c16fa71931561a75842
cux-maks <volcano020716@gmail.com>
2024-01-19 22:48:35 +0900

    add: backend 부분 추가

--- src/main/java/bcsd/backend/project/pokku/service/Portfolio/PortfolioServiceImpl.java
+++ src/main/java/bcsd/backend/project/pokku/service/Portfolio/PortfolioServiceImpl.java
@@ -1,12 +1,11 @@
 package bcsd.backend.project.pokku.service.Portfolio;
 
+import bcsd.backend.project.pokku.dao.SkillsBackendRepository;
 import bcsd.backend.project.pokku.dao.SkillsFrontendRepository;
+import bcsd.backend.project.pokku.dao.UserPortfolioSkillsBackendRepository;
 import bcsd.backend.project.pokku.dao.UserPortfolioSkillsFrontendRepository;
-import bcsd.backend.project.pokku.domain.SkillsFrontend;
-import bcsd.backend.project.pokku.domain.UserInfo;
-import bcsd.backend.project.pokku.domain.UserPortfolioSkillsFrontend;
+import bcsd.backend.project.pokku.domain.*;
 import bcsd.backend.project.pokku.dto.Portfolio.PortfolioRequest;
-import bcsd.backend.project.pokku.dto.Portfolio.PortfolioResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -22,20 +21,37 @@ public class PortfolioServiceImpl implements PortfolioService {
     private final UserPortfolioSkillsFrontendRepository userPortfolioSkillsFrontendRepository;
     private final SkillsFrontendRepository skillsFrontendRepository;
 
+    private final UserPortfolioSkillsBackendRepository userPortfolioSkillsBackendRepository;
+    private final SkillsBackendRepository skillsBackendRepository;
+
     @Override
     public Boolean addSkills(PortfolioRequest request) throws Exception{
 
-        if (request.getSkillsFrontendId() != null){
-            skillsFrontendRepository.findById(request.getSkillsFrontendId())
+        if (request.getCategory().equals("frontend")){
+            skillsFrontendRepository.findById(request.getSkillsId())
                     .orElseThrow(() -> new Exception("존재하지 않는 skills_frontend_id 입니다."));
 
             Optional<Long> exists = userPortfolioSkillsFrontendRepository.findEntityCnt(
                     UserInfo.builder().userId(request.getUserId()).build(),
-                    SkillsFrontend.builder().skillsFontendId(request.getSkillsFrontendId()).build());
+                    SkillsFrontend.builder().skillsFontendId(request.getSkillsId()).build());
 
             if(exists.get() == 0) {
                 userPortfolioSkillsFrontendRepository.save(UserPortfolioSkillsFrontend.builder()
-                        .skillsFrontend(SkillsFrontend.builder().skillsFontendId(request.getSkillsFrontendId()).build())
+                        .skillsFrontend(SkillsFrontend.builder().skillsFontendId(request.getSkillsId()).build())
+                        .userInfo(UserInfo.builder().userId(request.getUserId()).build())
+                        .build());
+            }
+        }else if(request.getCategory().equals("backend")){
+            skillsBackendRepository.findById(request.getSkillsId())
+                    .orElseThrow(() -> new Exception("존재하지 않는 skills_backend_id 입니다."));
+
+            Optional<Long> exists = userPortfolioSkillsBackendRepository.findEntityCnt(
+                    UserInfo.builder().userId(request.getUserId()).build(),
+                    SkillsBackend.builder().skillsBackendId(request.getSkillsId()).build());
+
+            if(exists.get() == 0) {
+                userPortfolioSkillsBackendRepository.save(UserPortfolioSkillsBackend.builder()
+                        .skillsBackend(SkillsBackend.builder().skillsBackendId(request.getSkillsId()).build())
                         .userInfo(UserInfo.builder().userId(request.getUserId()).build())
                         .build());
             }
@@ -46,12 +62,21 @@ public class PortfolioServiceImpl implements PortfolioService {
 
     @Override
     public Boolean deleteSkills(PortfolioRequest request) throws Exception{
-        UserPortfolioSkillsFrontend exists = userPortfolioSkillsFrontendRepository.findEntity(
-                UserInfo.builder().userId(request.getUserId()).build(),
-                SkillsFrontend.builder().skillsFontendId(request.getSkillsFrontendId()).build())
-                .orElseThrow(() -> new BadCredentialsException("유효하지 않은 데이터 입니다."));
+        if (request.getCategory().equals("frontend")) {
+            UserPortfolioSkillsFrontend exists = userPortfolioSkillsFrontendRepository.findEntity(
+                            UserInfo.builder().userId(request.getUserId()).build(),
+                            SkillsFrontend.builder().skillsFontendId(request.getSkillsId()).build())
+                    .orElseThrow(() -> new BadCredentialsException("유효하지 않은 데이터 입니다."));
 
-        userPortfolioSkillsFrontendRepository.deleteById(exists.getUserPortfolioSkillsFrontendId());
+            userPortfolioSkillsFrontendRepository.deleteById(exists.getUserPortfolioSkillsFrontendId());
+        }else if(request.getCategory().equals("backend")) {
+            UserPortfolioSkillsBackend exists = userPortfolioSkillsBackendRepository.findEntity(
+                            UserInfo.builder().userId(request.getUserId()).build(),
+                            SkillsBackend.builder().skillsBackendId(request.getSkillsId()).build())
+                    .orElseThrow(() -> new BadCredentialsException("유효하지 않은 데이터 입니다."));
+
+            userPortfolioSkillsBackendRepository.deleteById(exists.getUserPortfolioSkillsBackendId());
+        }
 
         return true;
     }
