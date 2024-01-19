0c03aa7a80b8808c8259dbe2b855aaa294ebf4ad
cux-maks <volcano020716@gmail.com>
2024-01-19 22:54:57 +0900

    add: UserPortfolioSkillsDeployment, UserPortfolioSkillsDeploymentRepository 추가

---
+++ src/main/java/bcsd/backend/project/pokku/dao/UserPortfolioSkillsDeploymentRepository.java
@@ -0,0 +1,19 @@
+package bcsd.backend.project.pokku.dao;
+
+import bcsd.backend.project.pokku.domain.*;
+import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserPortfolioSkillsDeploymentRepository extends JpaRepository<UserPortfolioSkillsDeployment, Long> {
+    @Query("SELECT count(p) FROM UserPortfolioSkillsDeployment p WHERE p.userInfo = :user_id and p.skillsDeployment = :skills_deployment_id")
+    Optional<Long> findEntityCnt(@Param("user_id") UserInfo user, @Param("skills_deployment_id") SkillsDeployment skillsDeployment);
+
+    @Query("SELECT p FROM UserPortfolioSkillsDeployment p WHERE p.userInfo = :user_id and p.skillsDeployment = :skills_deployment_id")
+    Optional<UserPortfolioSkillsDeployment> findEntity(@Param("user_id") UserInfo user, @Param("skills_deployment_id") SkillsDeployment skillsDeployment);
+}
