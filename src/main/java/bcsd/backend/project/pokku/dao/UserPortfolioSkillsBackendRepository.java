a1433cd48fef5942409911485825c3014f66146d
cux-maks <volcano020716@gmail.com>
2024-01-19 22:55:10 +0900

    add: UserPortfolioSkillsDeployment, UserPortfolioSkillsDeploymentRepository 추가

--- src/main/java/bcsd/backend/project/pokku/dao/UserPortfolioSkillsBackendRepository.java
+++ src/main/java/bcsd/backend/project/pokku/dao/UserPortfolioSkillsBackendRepository.java
@@ -4,9 +4,11 @@ import bcsd.backend.project.pokku.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserPortfolioSkillsBackendRepository extends JpaRepository<UserPortfolioSkillsBackend, Long> {
 
     @Query("SELECT count(p) FROM UserPortfolioSkillsBackend p WHERE p.userInfo = :user_id and p.skillsBackend = :skills_backend_id")
