cf05ef273f9b89a2aace78de8dd1637e9bc04a88
cux-maks <volcano020716@gmail.com>
2024-01-19 22:46:16 +0900

    add: UserPortfolioSkillsBackend, UserPortfolioSkillsBackendRepository 추가

---
+++ src/main/java/bcsd/backend/project/pokku/dao/UserPortfolioSkillsBackendRepository.java
@@ -0,0 +1,18 @@
+package bcsd.backend.project.pokku.dao;
+
+import bcsd.backend.project.pokku.domain.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserPortfolioSkillsBackendRepository extends JpaRepository<UserPortfolioSkillsBackend, Long> {
+
+    @Query("SELECT count(p) FROM UserPortfolioSkillsBackend p WHERE p.userInfo = :user_id and p.skillsBackend = :skills_backend_id")
+    Optional<Long> findEntityCnt(@Param("user_id") UserInfo user, @Param("skills_backend_id") SkillsBackend skillsBackend);
+
+    @Query("SELECT p FROM UserPortfolioSkillsBackend p WHERE p.userInfo = :user_id and p.skillsBackend = :skills_backend_id")
+    Optional<UserPortfolioSkillsBackend> findEntity(@Param("user_id") UserInfo user, @Param("skills_backend_id") SkillsBackend skillsBackend);
+
+}
