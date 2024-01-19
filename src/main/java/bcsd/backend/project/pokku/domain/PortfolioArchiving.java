a553e2d9f7e849f47d1d40960305b908d55ee9b1
cux-maks <volcano020716@gmail.com>
2024-01-20 00:30:40 +0900

    add: PortfolioArchiving, PortfolioArchivingRepository 추가

---
+++ src/main/java/bcsd/backend/project/pokku/domain/PortfolioArchiving.java
@@ -0,0 +1,33 @@
+package bcsd.backend.project.pokku.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@Table(name = "portfolio_archiving")
+@AllArgsConstructor
+@NoArgsConstructor
+public class PortfolioArchiving {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "portfolio_archiving_id")
+    private String portfolioArchivingId;
+
+    @Column(name = "archiving_name")
+    private String archivingName;
+
+    @Column(name = "archiving_explanation")
+    private String archivingExplanation;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private UserInfo userInfo;
+
+
+}
