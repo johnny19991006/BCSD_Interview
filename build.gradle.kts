6d0aa146599a7f996205855bdb8dcec9aaa84f0f
ImTotem <psb106305@gmail.com>
2024-01-19 16:55:41 +0900

    chore: jwt 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,11 @@ dependencies {
     // Spring Security
     implementation("org.springframework.boot:spring-boot-starter-security")
 
+	// Jwt
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
     // Validation
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
