3e630fa3a59b2a398e6a57137d8382bcb357cfdc
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-01-21 21:40:04 +0900

    change to record class

--- src/main/java/com/forum/forum_site/dto/SavePostDto.java
+++ src/main/java/com/forum/forum_site/dto/SavePostDto.java
@@ -1,29 +1,17 @@
 package com.forum.forum_site.dto;
 
 import com.forum.forum_site.domain.Post;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-public class SavePostDto {
-    @NotNull(message = "제목")
-    private String title;
 
-    @NotNull(message = "내용")
-    private String content;
-
-    private List<MultipartFile> uploadFiles;
+public record SavePostDto(@NotBlank(message = "제목을 입력해주세요") String title,
+                          @NotBlank(message = "내용을 입력해주세요") String content,
+                          List<MultipartFile> uploadFile) {
 
     public Post toEntity() {
-        return Post.builder()
-                .title(title)
-                .content(content)
-                .build();
+        return Post.builder().title(title).content(content).build();
     }
-
 }
