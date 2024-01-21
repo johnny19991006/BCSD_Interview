f8cd781630e4ec6dc9d91cd07fa3e83ff4b782d8
cux-maks <volcano020716@gmail.com>
2024-01-21 20:59:15 +0900

    add: /career 권한 추가

--- src/main/java/bcsd/backend/project/pokku/security/SecurityConfig.java
+++ src/main/java/bcsd/backend/project/pokku/security/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
                                 .requestMatchers("/download-img/**").hasAnyRole("User", "Admin")
                                 .requestMatchers("/portfolio").hasAnyRole("User", "Admin")
                                 .requestMatchers("/archiving").hasAnyRole("User", "Admin")
+                                .requestMatchers("/career").hasAnyRole("User", "Admin")
                                 .anyRequest().denyAll()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
