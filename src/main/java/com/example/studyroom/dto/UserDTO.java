88b97616f9abf5eb4ff46fd23a76e6dbf3abc273
dradnats1012 <herjebi1012@gmail.com>
2024-01-22 22:38:04 +0900

    feat : UserDTO 클래스 생성

--- src/main/java/com/example/studyroom/dto/UserDTO.java
+++ src/main/java/com/example/studyroom/dto/UserDTO.java
@@ -1,4 +1,13 @@
 package com.example.studyroom.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserDTO {
+    private int schoolId;
+    private String name;
+    private String password;
 }
