7172dbedd97c805398fe60f396601833da40d350
cux-maks <volcano020716@gmail.com>
2024-01-22 16:50:01 +0900

    add: PortfolioController 추가

---
+++ src/main/java/bcsd/backend/project/pokku/controller/PortfolioController.java
@@ -0,0 +1,9 @@
+package bcsd.backend.project.pokku.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/portfolio")
+public class PortfolioController {
+}
