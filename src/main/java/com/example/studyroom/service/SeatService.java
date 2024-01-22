7257cd71547ee0684ffdb79f098970d0b88e4ac6
dradnats1012 <herjebi1012@gmail.com>
2024-01-22 23:58:26 +0900

    feat : SeatService 클래스에 "좌석 선택" 메서드 구현

--- src/main/java/com/example/studyroom/service/SeatService.java
+++ src/main/java/com/example/studyroom/service/SeatService.java
@@ -2,12 +2,15 @@ package com.example.studyroom.service;
 
 import com.example.studyroom.domain.Room;
 import com.example.studyroom.domain.Seat;
+import com.example.studyroom.domain.User;
+import com.example.studyroom.dto.ChoiceSeatDTO;
 import com.example.studyroom.dto.InsertSeatDTO;
 import com.example.studyroom.repository.SeatRepository;
 import com.example.studyroom.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -39,6 +42,46 @@ public class SeatService {
 
     public void deleteSeat(Integer seatId) {
         seatRepository.delete(Seat.builder().seatId(seatId).build());
+    }
+
+    public Seat choiceSeat(ChoiceSeatDTO choiceSeatDTO) {
+
+        isUserUsing(choiceSeatDTO.getSchoolId());
+        isOccupied(choiceSeatDTO.getIsUsed());
+
+        return updateSeat(choiceSeatDTO);
+    }
+
+    private void isUserUsing(Integer schoolId) {
+        if (seatRepository.existsByUser(User.builder().schoolId(schoolId).build())) {
+            throw new IllegalArgumentException("이미 선택한 자리가 있습니다!");
+        }
+    }
+
+    private void isOccupied(boolean isUsed) {
+        if (isUsed) {
+            throw new IllegalArgumentException("이미 사용하고 있는 자리입니다!");
+        }
+    }
+
+    public Seat updateSeat(ChoiceSeatDTO choiceSeatDTO) {
+        LocalDateTime now = LocalDateTime.now();
+
+        Seat seat = seatRepository.findById(choiceSeatDTO.getSeatId()).orElse(null);
+
+        if (seat != null) {
+            seat.setSeatNum(choiceSeatDTO.getSeatNum());
+            seat.setIsUsed(true);
+            seat.setUser(User.builder().schoolId(choiceSeatDTO.getSchoolId()).build());
+            seat.setRoom(Room.builder().roomId(choiceSeatDTO.getRoomId()).build());
+            seat.setStartTime(now);
+            seat.setEndTime(now.plusHours(3));
+
+            seatRepository.save(seat);
+            return seat;
+        }
+
+        return null;
     }
 
 }
