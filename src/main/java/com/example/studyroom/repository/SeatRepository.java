c84f4f2bd874bd0ec2d797bb88020012e4ac4c96
dradnats1012 <herjebi1012@gmail.com>
2024-01-22 19:03:33 +0900

    feat : SeatRepository 인터페이스 생성

--- src/main/java/com/example/studyroom/repository/SeatRepository.java
+++ src/main/java/com/example/studyroom/repository/SeatRepository.java
@@ -1,7 +1,9 @@
 package com.example.studyroom.repository;
 
 import com.example.studyroom.domain.Seat;
+import com.example.studyroom.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeatRepository extends JpaRepository<Seat, Integer> {
+    Boolean existsByUser(User user);
 }
