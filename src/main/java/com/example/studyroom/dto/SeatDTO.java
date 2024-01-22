43624b3d038db874ea3e6bb77dd44cde89d95654
dradnats1012 <herjebi1012@gmail.com>
2024-01-22 22:37:47 +0900

    feat : SeatDTO 클래스 생성

--- src/main/java/com/example/studyroom/dto/SeatDTO.java
+++ src/main/java/com/example/studyroom/dto/SeatDTO.java
@@ -1,4 +1,22 @@
 package com.example.studyroom.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class SeatDTO {
+    private Integer seatId;
+    private Integer roomId;
+    private Integer seatNum;
+    private Integer schoolId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Boolean isUsed;
 }
