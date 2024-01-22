1a7cfdb9725f1ae452a2af721aee32fe514e7ea3
dradnats1012 <herjebi1012@gmail.com>
2024-01-22 22:37:56 +0900

    feat : RoomDTO 클래스 생성

--- src/main/java/com/example/studyroom/dto/RoomDTO.java
+++ src/main/java/com/example/studyroom/dto/RoomDTO.java
@@ -1,4 +1,17 @@
 package com.example.studyroom.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class RoomDTO {
+    private int roomId;
+    private int remainSeats;
+    private int seats;
+    private int usedSeats;
 }
