235fe712a0ee2501efe8d7ffd6b1ecc3ec2bba41
dradnats1012 <herjebi1012@gmail.com>
2024-01-22 23:53:13 +0900

    feat : SeatController 클래스 생성

---
+++ src/main/java/com/example/studyroom/controller/SeatController.java
@@ -0,0 +1,41 @@
+package com.example.studyroom.controller;
+
+import com.example.studyroom.domain.Seat;
+import com.example.studyroom.dto.ChangeSeatDTO;
+import com.example.studyroom.dto.ChoiceSeatDTO;
+import com.example.studyroom.dto.InsertSeatDTO;
+import com.example.studyroom.service.SeatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/seats")
+public class SeatController {
+    private final SeatService seatService;
+
+    public SeatController(SeatService seatService) {
+        this.seatService = seatService;
+    }
+
+    @PostMapping("")
+    public Seat insertSeat(@RequestBody InsertSeatDTO insertSeatDTO){
+        return seatService.insertSeat(insertSeatDTO);
+    }
+
+    @GetMapping("")
+    public List<Seat> getAllSeats() {
+        return seatService.getAllSeats();
+    }
+
+    @GetMapping("/{seatId}")
+    public Seat getSeatBySeatId(@PathVariable int seatId) {
+        return seatService.getSeatBySeatID(seatId);
+    }
+
+    @DeleteMapping("/{seatId}")
+    public void deleteSeat( @PathVariable int seatId) {
+        seatService.deleteSeat(seatId);
+    }
+}
