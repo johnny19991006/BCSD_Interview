9ff19eb2beaf33d397ac96c878448ed31d20afd7
cux-maks <volcano020716@gmail.com>
2024-01-22 13:15:25 +0900

    deleted: 기존 서비스 파일 삭제

--- src/main/java/bcsd/backend/project/pokku/service/SignIn/SignInService.java
+++
@@ -1,8 +0,0 @@
-package bcsd.backend.project.pokku.service.SignIn;
-
-import bcsd.backend.project.pokku.dto.SignIn.SignInRequest;
-import bcsd.backend.project.pokku.dto.SignIn.SignInResponse;
-
-public interface SignInService {
-    public SignInResponse login(SignInRequest request) throws Exception;
-}
--- src/main/java/bcsd/backend/project/pokku/service/SignUp/SignUpService.java
+++
@@ -1,7 +0,0 @@
-package bcsd.backend.project.pokku.service.SignUp;
-
-import bcsd.backend.project.pokku.dto.SignUp.SignUpRequest;
-
-public interface SignUpService {
-    public boolean register(SignUpRequest request) throws Exception;
-}
