76d33d50495166bc48c3eb83acdcf82e15bb1810
dradnats1012 <herjebi1012@gmail.com>
2024-01-22 19:01:59 +0900

    feat : User 도메인 클래스 수정

--- src/main/java/com/example/studyroom/domain/User.java
+++ src/main/java/com/example/studyroom/domain/User.java
@@ -1,22 +1,21 @@
 package com.example.studyroom.domain;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Builder
 @Table(name = "users")
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 @Setter
 public class User {
     @Id
     @Column(name = "school_id")
-    private int schoolId;
+    private Integer schoolId;
 
     @Column
     @NotNull
@@ -28,8 +27,6 @@ public class User {
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "school_id")
+    @Nullable
     private Seat seat;
-
-    public User() {
-    }
 }
