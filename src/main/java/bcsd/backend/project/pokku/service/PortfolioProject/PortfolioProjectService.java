eb5783481fb1653c051c16218168f45a2f3bce29
cux-maks <volcano020716@gmail.com>
2024-01-24 00:21:08 +0900

    deleted: 사용하지 않는 파일 삭제

--- src/main/java/bcsd/backend/project/pokku/service/PortfolioProject/PortfolioProjectService.java
+++
@@ -1,8 +0,0 @@
-package bcsd.backend.project.pokku.service.PortfolioProject;
-
-import bcsd.backend.project.pokku.dto.PortfolioProject.PortfolioProjectRequest;
-
-public interface PortfolioProjectService {
-    public Boolean addProject(PortfolioProjectRequest request) throws Exception;
-    public Boolean deleteProject(PortfolioProjectRequest request) throws Exception;
-}
