9c228c33ae1c2bf655468e06fcf9411154d70ce6
cux-maks <volcano020716@gmail.com>
2024-01-24 00:23:15 +0900

    deleted: 사용하지 않는 파일 삭제

--- src/main/java/bcsd/backend/project/pokku/service/UserInfoSNS/UserInfoSNSService.java
+++
@@ -1,9 +0,0 @@
-package bcsd.backend.project.pokku.service.UserInfoSNS;
-
-import bcsd.backend.project.pokku.dto.UserInfoSNS.UserInfoSNSRequest;
-import bcsd.backend.project.pokku.dto.UserInfoSNS.UserInfoSNSResponse;
-
-public interface UserInfoSNSService {
-    public UserInfoSNSResponse findSNS(UserInfoSNSRequest request) throws Exception;
-    public boolean UpdateSNS(UserInfoSNSRequest request) throws Exception;
-}
