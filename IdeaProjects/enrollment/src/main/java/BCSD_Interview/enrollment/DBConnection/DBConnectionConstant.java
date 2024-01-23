dcb01d1c9fcbaf75e38edbafa9d45b033e59bad8
wjm0423 <wjm0423@naver.com>
2024-01-24 01:32:37 +0900

    DBConnection 추가

---
+++ IdeaProjects/enrollment/src/main/java/BCSD_Interview/enrollment/DBConnection/DBConnectionConstant.java
@@ -0,0 +1,7 @@
+package BCSD_Interview.enrollment.DBConnection;
+
+public class DBConnectionConstant {
+    public static final String URL = "jdbc:mysql://localhost:3306/enrollment";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "dkfzkeldjtm24";
+}
