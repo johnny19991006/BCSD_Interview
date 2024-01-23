bc761e7d7e64b6596973815f6d4d9d9fecef45b3
duehee <149302959+duehee@users.noreply.github.com>
2024-01-23 13:23:08 +0900

    test : 커밋 오류 체크

---
+++ src/main/java/AcademicManagement/BCSDproject/BcsDprojectApplication.java
@@ -0,0 +1,13 @@
+package AcademicManagement.BCSDproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcsDprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcsDprojectApplication.class, args);
+	}
+
+}
