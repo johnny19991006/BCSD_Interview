b52440f54b0effca689b8546fa3cb48e5593ff07
cux-maks <volcano020716@gmail.com>
2024-01-24 00:16:08 +0900

    deleted: 사용하지 않는 파일 삭제

--- src/main/java/bcsd/backend/project/pokku/service/PortfolioArchiving/PortfolioArchivingService.java
+++
@@ -1,8 +0,0 @@
-package bcsd.backend.project.pokku.service.PortfolioArchiving;
-
-import bcsd.backend.project.pokku.dto.PortfolioArchiving.PortfolioArchivingRequest;
-
-public interface PortfolioArchivingService {
-    public Boolean addArchiving(PortfolioArchivingRequest request) throws Exception;
-    public Boolean deleteArchiving(PortfolioArchivingRequest request) throws Exception;
-}
