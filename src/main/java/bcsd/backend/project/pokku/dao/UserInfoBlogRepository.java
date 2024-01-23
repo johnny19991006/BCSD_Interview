353dbaa1cce90b5dd711a58a11a15434b21ff5a6
cux-maks <volcano020716@gmail.com>
2024-01-24 00:23:01 +0900

    modified: userId를 이용한 delete 쿼리 추가

--- src/main/java/bcsd/backend/project/pokku/dao/UserInfoBlogRepository.java
+++ src/main/java/bcsd/backend/project/pokku/dao/UserInfoBlogRepository.java
@@ -3,6 +3,7 @@ package bcsd.backend.project.pokku.dao;
 import bcsd.backend.project.pokku.domain.UserInfo;
 import bcsd.backend.project.pokku.domain.UserInfoBlog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,5 +15,9 @@ public interface UserInfoBlogRepository extends JpaRepository<UserInfoBlog, Stri
 
     @Query("SELECT p FROM UserInfoBlog p WHERE p.userInfo = :user_id")
     Optional<UserInfoBlog> findByUserId(@Param("user_id") UserInfo user);
+
+    @Query("DELETE FROM UserInfoBlog p WHERE p.userInfo = :user_id")
+    @Modifying
+    void deleteByUserId(@Param("user_id") UserInfo user);
 
 }
--- src/main/java/bcsd/backend/project/pokku/dao/UserInfoInstagramRepository.java
+++ src/main/java/bcsd/backend/project/pokku/dao/UserInfoInstagramRepository.java
@@ -3,6 +3,7 @@ package bcsd.backend.project.pokku.dao;
 import bcsd.backend.project.pokku.domain.UserInfo;
 import bcsd.backend.project.pokku.domain.UserInfoInstagram;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,5 +15,9 @@ public interface UserInfoInstagramRepository extends JpaRepository<UserInfoInsta
 
     @Query("SELECT p FROM UserInfoInstagram p WHERE p.userInfo = :user_id")
     Optional<UserInfoInstagram> findByUserId(@Param("user_id") UserInfo user);
+
+    @Query("DELETE FROM UserInfoInstagram p WHERE p.userInfo = :user_id")
+    @Modifying
+    void deleteByUserId(@Param("user_id") UserInfo user);
 
 }
