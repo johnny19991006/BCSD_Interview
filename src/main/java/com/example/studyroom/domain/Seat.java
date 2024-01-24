ab6006be8b4c472780a331549b8420c86890af4b
dradnats1012 <herjebi1012@gmail.com>
2024-01-24 16:28:57 +0900

    feat : Seat @Setter, @AllArgsConstructor 삭제

--- src/main/java/com/example/studyroom/domain/Seat.java
+++ src/main/java/com/example/studyroom/domain/Seat.java
@@ -2,7 +2,8 @@ package com.example.studyroom.domain;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -10,11 +11,11 @@ import java.time.LocalDateTime;
 @Entity
 @Builder
 @Table(name = "seats")
-@AllArgsConstructor
-@RequiredArgsConstructor
 @Getter
-@Setter
 public class Seat {
+    public Seat() {
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "seat_id")
@@ -23,8 +24,9 @@ public class Seat {
     @Column(name = "seat_number")
     private Integer seatNum;
 
-    @Column(name = "is_used", columnDefinition = "boolean default false")
-    private Boolean isUsed;
+    @Builder.Default
+    @Column(name = "is_used")
+    private Boolean isUsed = false;
 
     @Column(name = "start_time")
     @CreationTimestamp
@@ -41,4 +43,44 @@ public class Seat {
     @OneToOne
     @JoinColumn(name = "school_id")
     private User user;
+
+    public Seat(Integer seatNum, Room room) {
+        this.seatNum = seatNum;
+        this.room = room;
+    }
+
+    public Seat(Integer seatId) {
+        this.seatId = seatId;
+    }
+
+    public Seat(Integer seatId, Integer seatNum, Boolean isUsed, LocalDateTime startTime, LocalDateTime endTime,
+                Room room, User user) {
+        this.seatId = seatId;
+        this.seatNum = seatNum;
+        this.isUsed = isUsed;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.room = room;
+        this.user = user;
+    }
+
+    public void endSeatUpdate() {
+
+        this.user = null;
+        this.startTime = null;
+        this.endTime = null;
+        this.isUsed = false;
+    }
+
+    public void choiceSeatUpdate(Integer seatNum, User user, Room room) {
+
+        LocalDateTime now = LocalDateTime.now();
+
+        this.seatNum = seatNum;
+        this.isUsed = true;
+        this.user = user;
+        this.room = room;
+        this.startTime = now;
+        this.endTime = now.plusHours(3);
+    }
 }
