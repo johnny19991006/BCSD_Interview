3bfe31bbfd7f66bc25538ace179fde4b5926583c
dradnats1012 <herjebi1012@gmail.com>
2024-01-24 22:14:51 +0900

    feat : EndSeatDTo 클래스 CancleSeatDTO로 rename

--- src/main/java/com/example/studyroom/dto/CancleSeatDTO.java
+++ src/main/java/com/example/studyroom/dto/CancleSeatDTO.java
@@ -3,8 +3,7 @@ package com.example.studyroom.dto;
 import lombok.Getter;
 
 @Getter
-public class EndSeatDTO {
+public class CancleSeatDTO {
     Integer seatId;
     Integer schoolId;
-    Boolean isUsed;
 }
