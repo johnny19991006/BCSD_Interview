a4f4d3eaf94c6b3178ff495516a2b476faae97ea
dradnats1012 <herjebi1012@gmail.com>
2024-01-24 16:47:59 +0900

    feat : jpa 메서드 생성

--- src/main/java/com/example/studyroom/repository/SeatRepository.java
+++ src/main/java/com/example/studyroom/repository/SeatRepository.java
@@ -3,7 +3,21 @@ package com.example.studyroom.repository;
 import com.example.studyroom.domain.Seat;
 import com.example.studyroom.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SeatRepository extends JpaRepository<Seat, Integer> {
     Boolean existsByUser(User user);
+
+    @Query("SELECT s.seatId FROM Seat s WHERE s.user.schoolId = :schoolId")
+    Integer findSeatIdByUserSchoolId(@Param("schoolId") Integer schoolId);
+
+    @Query("SELECT s.room.roomId FROM Seat s WHERE s.seatId = :seatId")
+    Integer findRoomIdBySeatId(@Param("seatId") Integer seatId);
+
+    @Query("SELECT s.isUsed FROM Seat s WHERE s.seatId = :seatId")
+    Boolean findIsUsedBySeatId(@Param("seatId") Integer seatId);
+
+    @Query("SELECT s.seatNum FROM Seat s WHERE s.seatId = :seatId")
+    Integer findSeatNumBySeatId(@Param("seatId") Integer seatId);
 }
--- src/main/java/com/example/studyroom/repository/UserRepository.java
+++ src/main/java/com/example/studyroom/repository/UserRepository.java
@@ -2,10 +2,9 @@ package com.example.studyroom.repository;
 
 import com.example.studyroom.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Boolean existsBySchoolId(int schoolId);
+
+    User findBySchoolId(int schoolId);
 }
