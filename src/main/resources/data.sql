e42afbcc3ed1acb8479ef64ae644600d89e2769d
ImTotem <psb106305@gmail.com>
2024-01-25 11:50:50 +0900

    chore: auto-ddl 적용

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,24 +1,11 @@
 # CREATE
-INSERT INTO `users` (`email`, `password`)
-VALUES ('admin@test.com', 'password'),
-       ('admin2@test.com', 'password2'),
-       ('user@test.com', 'user'),
-       ('user2@test.com', 'user2');
+INSERT INTO `users` (`email`, `password`, `role`, `created_at`)
+VALUES ('admin@test.com', 'password', 1, NOW(6)),
+       ('admin2@test.com', 'password2', 1, NOW(6)),
+       ('user@test.com', 'user', 0, NOW(6)),
+       ('user2@test.com', 'user2', 0, NOW(6));
 
-INSERT INTO `admins` (`id`) SELECT `id` FROM `users` WHERE id in (1, 2);
+# ADMIN UPDATE
+# INSERT INTO `role` (`id`) SELECT `id` FROM `users` WHERE id in (1, 2);
 
-# UPDATE
-UPDATE `users` SET `password` = 'updatePassword2' WHERE id = 2;
-UPDATE `users` SET `password` = 'updateUser2' WHERE id = 4;
-
-# READ
 SELECT * FROM `users`;
-SELECT * FROM `admins`;
-
-# DELETE
-# DELETE FROM `users` WHERE id = 2;
-# DELETE FROM `users` WHERE id = 4;
-
-# READ
-# SELECT * FROM `users`;
-# SELECT * FROM `admins`;
