ea0c21de39ddeff8e08cd8ce05a5eb266f099dd4
ImTotem <psb106305@gmail.com>
2024-01-25 11:18:27 +0900

    feat: 토큰 인증/인가 로직 추가

--- src/main/java/io/github/imtotem/shortly/security/SecurityConfig.java
+++ src/main/java/io/github/imtotem/shortly/security/SecurityConfig.java
@@ -1,12 +1,11 @@
 package io.github.imtotem.shortly.security;
 
-import io.github.imtotem.shortly.security.jwt.JwtFilter;
+import io.github.imtotem.shortly.security.jwt.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -15,36 +14,47 @@ import org.springframework.security.config.annotation.web.configurers.HttpBasicC
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
-    private final JwtFilter jwtFilter;
+    private final JwtAuthFilter jwtAuthFilter;
+
+    private final AuthenticationEntryPoint userAuthenticationEntryPoint;
+    private final AccessDeniedHandler userAccessDeniedHandler;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .cors(Customizer.withDefaults())
-            .httpBasic(HttpBasicConfigurer::disable) // rest api 이므로 기본설정 사용안함. 기본설정은 비인증시 로그인폼 화면으로 리다이렉트 된다.
             .csrf(CsrfConfigurer::disable) // rest api 이므로 csrf 보안이 필요 없음. disable
+            .cors(Customizer.withDefaults())
+
+            .httpBasic(HttpBasicConfigurer::disable) // rest api 이므로 기본설정 사용안함. 기본설정은 비인증시 로그인폼 화면으로 리다이렉트 된다.
             .formLogin(FormLoginConfigurer::disable)
+
             // jwt token으로 인증하므로 세션은 필요없으므로 생성안함.
             .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(authorize -> authorize  // 다음 리퀘스트에 대한 사용권한 체크
-                    .requestMatchers("/api/users/**").permitAll()
-                    .anyRequest().authenticated()
+
+            // jwt token 필터
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+
+            .exceptionHandling(exceptionHandling -> exceptionHandling
+                    .authenticationEntryPoint(userAuthenticationEntryPoint)
+                    .accessDeniedHandler(userAccessDeniedHandler)
             )
-            // jwt token 필터를 id/password 인증 필터 전에 넣는다
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+            .authorizeHttpRequests(authorize -> authorize  // 다음 리퀘스트에 대한 사용권한 체크
+                    .requestMatchers("/api/**").permitAll()
+                    .anyRequest().authenticated()
+            );
 
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
---
+++ src/main/java/io/github/imtotem/shortly/security/UserAccessDeniedHandler.java
@@ -0,0 +1,43 @@
+package io.github.imtotem.shortly.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.imtotem.shortly.dto.Error;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j(topic = "FORBIDDEN_EXCEPTION_HANDLER")
+@RequiredArgsConstructor
+@Component
+public class UserAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        log.error("No Authorities", accessDeniedException);
+
+        Error error = Error.builder()
+                .status(HttpStatus.FORBIDDEN.value())
+                .message(accessDeniedException.getMessage())
+                .build();
+
+        String responseBody = objectMapper.writeValueAsString(error);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(responseBody);
+    }
+}
