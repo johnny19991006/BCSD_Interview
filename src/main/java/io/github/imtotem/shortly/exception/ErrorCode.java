2af37ba4a9c3269d6f6b51daf53dd178bc74074c
ImTotem <psb106305@gmail.com>
2024-01-25 11:11:40 +0900

    refactor: 에러 코드 추가

--- src/main/java/io/github/imtotem/shortly/exception/ErrorCode.java
+++ src/main/java/io/github/imtotem/shortly/exception/ErrorCode.java
@@ -10,6 +10,9 @@ public enum ErrorCode {
     //400 BAD_REQUEST 잘못된 요청
     INVALID_PARAMETER(400, "잘못된 요청입니다."),
 
+    //401 BAD_CREDENTIAL
+    WRONG_PASSWORD(401, "비밀번호가 일치하지 않습니다."),
+
     //404 NOT_FOUND 잘못된 리소스 접근
     EMAIL_NOT_FOUND(404, "이메일을 찾을 수 없습니다."),
     USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
