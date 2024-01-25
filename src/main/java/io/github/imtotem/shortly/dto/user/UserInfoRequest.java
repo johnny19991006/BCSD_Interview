45538be669a23bcfd80bf329e5902bd7993e79de
ImTotem <psb106305@gmail.com>
2024-01-25 11:06:48 +0900

    refactor: 유효성 검증 어노테이션 추가

--- src/main/java/io/github/imtotem/shortly/dto/user/UserInfoRequest.java
+++ src/main/java/io/github/imtotem/shortly/dto/user/UserInfoRequest.java
@@ -1,11 +1,17 @@
 package io.github.imtotem.shortly.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserInfoRequest {
+
+    @Email
+    @NotBlank
     private String email;
+
     private String password;
 }
