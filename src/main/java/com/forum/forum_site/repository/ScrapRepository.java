e706e321444fa95bfb82248d19677145e903acc3
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-01-26 22:38:52 +0900

    add findAllByAuthor function to scrap repository

--- src/main/java/com/forum/forum_site/repository/ScrapRepository.java
+++ src/main/java/com/forum/forum_site/repository/ScrapRepository.java
@@ -6,8 +6,10 @@ import com.forum.forum_site.domain.Scrap;
 import com.forum.forum_site.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ScrapRepository extends JpaRepository<Scrap, Integer> {
     Optional<Scrap> findByAuthorAndPost(User user, Post post);
+    List<Scrap> findAllByAuthor(User user);
 }
