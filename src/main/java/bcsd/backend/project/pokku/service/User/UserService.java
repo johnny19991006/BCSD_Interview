fe4e610381dc1526a627f4a010ecbc0c2a562496
cux-maks <volcano020716@gmail.com>
2024-02-02 17:22:14 +0900

    modified: RuntimeException으로 변경 및 customException 사용

--- src/main/java/bcsd/backend/project/pokku/service/User/UserService.java
+++ src/main/java/bcsd/backend/project/pokku/service/User/UserService.java
@@ -6,23 +6,23 @@ import bcsd.backend.project.pokku.dto.UserSNS.UserSNSRequest;
 import bcsd.backend.project.pokku.dto.UserSNS.UserSNSResponse;
 
 public interface UserService {
-    public UserResponse findUsers(String userId) throws Exception;
-    public boolean DeleteUsers(String userId) throws Exception;
-    public boolean UpdateUsers(String userId, UserRequest request) throws Exception;
+    public UserResponse findUsers(String userId) throws RuntimeException;
+    public boolean DeleteUsers(String userId) throws RuntimeException;
+    public boolean UpdateUsers(String userId, UserRequest request) throws RuntimeException;
 
-    public UserSNSResponse findBlog(String userId) throws Exception;
-    public UserSNSResponse findGithub(String userId) throws Exception;
-    public UserSNSResponse findInstagram(String userId) throws Exception;
+    public UserSNSResponse findBlog(String userId) throws RuntimeException;
+    public UserSNSResponse findGithub(String userId) throws RuntimeException;
+    public UserSNSResponse findInstagram(String userId) throws RuntimeException;
 
-    public Boolean addBlog(String userId, UserSNSRequest request) throws Exception;
-    public Boolean addGithub(String userId, UserSNSRequest request) throws Exception;
-    public Boolean addInstagram(String userId, UserSNSRequest request) throws Exception;
+    public Boolean addBlog(String userId, UserSNSRequest request) throws RuntimeException;
+    public Boolean addGithub(String userId, UserSNSRequest request) throws RuntimeException;
+    public Boolean addInstagram(String userId, UserSNSRequest request) throws RuntimeException;
 
-    public Boolean updateBlog(String userId, UserSNSRequest request) throws Exception;
-    public Boolean updateGithub(String userId, UserSNSRequest request) throws Exception;
-    public Boolean updateInstagram(String userId, UserSNSRequest request) throws Exception;
+    public Boolean updateBlog(String userId, UserSNSRequest request) throws RuntimeException;
+    public Boolean updateGithub(String userId, UserSNSRequest request) throws RuntimeException;
+    public Boolean updateInstagram(String userId, UserSNSRequest request) throws RuntimeException;
 
-    public Boolean deleteBlog(String userId) throws Exception;
-    public Boolean deleteGithub(String userId) throws Exception;
-    public Boolean deleteInstagram(String userId) throws Exception;
+    public Boolean deleteBlog(String userId) throws RuntimeException;
+    public Boolean deleteGithub(String userId) throws RuntimeException;
+    public Boolean deleteInstagram(String userId) throws RuntimeException;
 }
