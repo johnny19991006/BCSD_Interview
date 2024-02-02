180be0f2093739e84130252212533098e79f59a0
cux-maks <volcano020716@gmail.com>
2024-02-02 18:56:06 +0900

    modified: requestbody 값 null관련 예외처리

--- src/main/java/bcsd/backend/project/pokku/controller/PortfolioController.java
+++ src/main/java/bcsd/backend/project/pokku/controller/PortfolioController.java
@@ -12,6 +12,8 @@ import bcsd.backend.project.pokku.dto.PortfolioProject.PortfolioProjectResponse;
 import bcsd.backend.project.pokku.dto.PortfolioSkills.PortfolioSkillsListResponse;
 import bcsd.backend.project.pokku.dto.PortfolioSkills.PortfolioSkillsRequest;
 import bcsd.backend.project.pokku.dto.PortfolioSkills.PortfolioSkillsResponse;
+import bcsd.backend.project.pokku.exception.NullValueException.NullValueException;
+import bcsd.backend.project.pokku.exception.ResCode;
 import bcsd.backend.project.pokku.service.Portfolio.PortfolioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -32,97 +34,184 @@ public class PortfolioController {
     }
 
     @GetMapping(value = "/about/{id}")
-    public ResponseEntity<PortfolioAboutResponse> findPortfolioAbout(@PathVariable(name = "id") String userId) throws Exception{
+    public ResponseEntity<PortfolioAboutResponse> findPortfolioAbout(@PathVariable(name = "id") String userId) throws RuntimeException{
         return new ResponseEntity<>(portfolioService.findPortfolioAbout(userId), HttpStatus.OK);
     }
 
     @PutMapping(value = "/about/{id}")
-    public ResponseEntity<Boolean> updatePortfolioAbout(@PathVariable(name = "id") String userId, @RequestBody PortfolioAboutRequest request) throws Exception{
+    public ResponseEntity<Boolean> updatePortfolioAbout(@PathVariable(name = "id") String userId, @RequestBody PortfolioAboutRequest request) throws RuntimeException{
+        if (request.getUserNameVisible() == null){
+            throw new NullValueException("userNameVisible값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getUserEmailVisible() == null){
+            throw new NullValueException("userEmailVisible값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getUserEducationVisible() == null){
+            throw new NullValueException("userEducationVisible값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getUserTelVisible() == null){
+            throw new NullValueException("userTelVisible값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(portfolioService.updatePortfolioAbout(userId, request), HttpStatus.OK);
     }
 
     @GetMapping(value = "/skills/{id}")
-    public ResponseEntity<PortfolioSkillsResponse> findSkills(@PathVariable(name = "id") String userId) throws Exception{
+    public ResponseEntity<PortfolioSkillsResponse> findSkills(@PathVariable(name = "id") String userId) throws RuntimeException{
         return new ResponseEntity<>(portfolioService.findSkills(userId), HttpStatus.OK);
     }
 
     @GetMapping(value = "/skillslist/{category}")
-    public ResponseEntity<List<PortfolioSkillsListResponse>> findSkillsList(@PathVariable(name = "category") String category) throws Exception{
+    public ResponseEntity<List<PortfolioSkillsListResponse>> findSkillsList(@PathVariable(name = "category") String category) throws RuntimeException{
         return new ResponseEntity<>(portfolioService.findSkillsList(category), HttpStatus.OK);
     }
 
     @PostMapping(value = "/skills/{id}")
-    public ResponseEntity<Boolean> addSKills(@PathVariable(name = "id") String userId, @RequestBody PortfolioSkillsRequest request) throws Exception{
+    public ResponseEntity<Boolean> addSKills(@PathVariable(name = "id") String userId, @RequestBody PortfolioSkillsRequest request) throws RuntimeException{
+        if (request.getCategory() == null || request.getCategory().equals("")){
+            throw new NullValueException("category값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getSkillsId() == null){
+            throw new NullValueException("skillsId값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(portfolioService.addSkills(userId, request), HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/skills/{id}")
-    public ResponseEntity<Boolean> deleteSKills(@PathVariable(name = "id") String userId, @RequestBody PortfolioSkillsRequest request) throws Exception{
+    public ResponseEntity<Boolean> deleteSKills(@PathVariable(name = "id") String userId, @RequestBody PortfolioSkillsRequest request) throws RuntimeException{
+        if (request.getCategory() == null || request.getCategory().equals("")){
+            throw new NullValueException("category값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getSkillsId() == null){
+            throw new NullValueException("skillsId값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(portfolioService.deleteSkills(userId, request), HttpStatus.OK);
     }
 
     @GetMapping(value = "/archiving/{id}")
-    public ResponseEntity<List<PortfolioArchivingResponse>> findArchiving(@PathVariable(name = "id") String userId) throws Exception{
+    public ResponseEntity<List<PortfolioArchivingResponse>> findArchiving(@PathVariable(name = "id") String userId) throws RuntimeException{
         return new ResponseEntity<>(portfolioService.findArchiving(userId), HttpStatus.OK);
     }
 
     @PostMapping(value = "/archiving/{id}")
-    public ResponseEntity<Boolean> addArchiving(@PathVariable(name = "id") String userId, @RequestBody PortfolioArchivingRequest request) throws Exception{
+    public ResponseEntity<Boolean> addArchiving(@PathVariable(name = "id") String userId, @RequestBody PortfolioArchivingRequest request) throws RuntimeException{
+        if (request.getArchivingName() == null || request.getArchivingName().equals("")){
+            throw new NullValueException("archivingName값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getArchivingExplanation() == null || request.getArchivingExplanation().equals("")){
+            throw new NullValueException("archivingExplanation값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(portfolioService.addArchiving(userId, request), HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/archiving/{id}")
-    public ResponseEntity<Boolean> deleteArchiving(@PathVariable(name = "id") String userId, @RequestBody PortfolioArchivingRequest request) throws Exception{
+    public ResponseEntity<Boolean> deleteArchiving(@PathVariable(name = "id") String userId, @RequestBody PortfolioArchivingRequest request) throws RuntimeException{
+        if (request.getArchivingName() == null || request.getArchivingName().equals("")){
+            throw new NullValueException("archivingName값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getArchivingExplanation() == null || request.getArchivingExplanation().equals("")){
+            throw new NullValueException("archivingExplanation값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getPortfolioArchivingId() == null){
+            throw new NullValueException("portfolioArchivingId값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(portfolioService.deleteArchiving(userId, request), HttpStatus.OK);
     }
 
     @PutMapping(value = "/archiving/{id}")
-    public ResponseEntity<Boolean> updateArchiving(@PathVariable(name = "id") String userId, @RequestBody PortfolioArchivingRequest request) throws Exception{
+    public ResponseEntity<Boolean> updateArchiving(@PathVariable(name = "id") String userId, @RequestBody PortfolioArchivingRequest request) throws RuntimeException{
+        if (request.getArchivingName() == null || request.getArchivingName().equals("")){
+            throw new NullValueException("archivingName값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getArchivingExplanation() == null || request.getArchivingExplanation().equals("")){
+            throw new NullValueException("archivingExplanation값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getPortfolioArchivingId() == null){
+            throw new NullValueException("portfolioArchivingId값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(portfolioService.deleteArchiving(userId, request), HttpStatus.OK);
     }
 
     @GetMapping(value = "/project/{id}")
-    public ResponseEntity<List<PortfolioProjectResponse>> findProject(@PathVariable(name = "id") String userId) throws Exception{
+    public ResponseEntity<List<PortfolioProjectResponse>> findProject(@PathVariable(name = "id") String userId) throws RuntimeException{
         return new ResponseEntity<>(portfolioService.findProject(userId), HttpStatus.OK);
     }
 
     @PostMapping(value = "/project/{id}")
-    public ResponseEntity<Boolean> addProject(@PathVariable(name = "id") String userId, @RequestBody PortfolioProjectRequest request) throws Exception{
+    public ResponseEntity<Boolean> addProject(@PathVariable(name = "id") String userId, @RequestBody PortfolioProjectRequest request) throws RuntimeException{
+        if (request.getProjectName() == null || request.getProjectName().equals("")){
+            throw new NullValueException("projectName값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getProjectExplanation() == null || request.getProjectExplanation().equals("")){
+            throw new NullValueException("projectExplanation값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(portfolioService.addProject(userId, request), HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/project/{id}")
-    public ResponseEntity<Boolean> deleteProject(@PathVariable(name = "id") String userId, @RequestBody PortfolioProjectRequest request) throws Exception{
+    public ResponseEntity<Boolean> deleteProject(@PathVariable(name = "id") String userId, @RequestBody PortfolioProjectRequest request) throws RuntimeException{
+        if (request.getProjectName() == null || request.getProjectName().equals("")){
+            throw new NullValueException("projectName값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getProjectExplanation() == null || request.getProjectExplanation().equals("")){
+            throw new NullValueException("projectExplanation값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getPortfolioProjectId() == null){
+            throw new NullValueException("portfolioProjectId값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(portfolioService.deleteProject(userId, request), HttpStatus.OK);
     }
 
     @PutMapping(value = "/project/{id}")
-    public ResponseEntity<Boolean> updateProject(@PathVariable(name = "id") String userId, @RequestBody PortfolioProjectRequest request) throws Exception{
+    public ResponseEntity<Boolean> updateProject(@PathVariable(name = "id") String userId, @RequestBody PortfolioProjectRequest request) throws RuntimeException{
+        if (request.getProjectName() == null || request.getProjectName().equals("")){
+            throw new NullValueException("projectName값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getProjectExplanation() == null || request.getProjectExplanation().equals("")){
+            throw new NullValueException("projectExplanation값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getPortfolioProjectId() == null){
+            throw new NullValueException("portfolioProjectId값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(portfolioService.updateProject(userId, request), HttpStatus.OK);
     }
 
     @GetMapping(value = "/career/{id}")
-    public ResponseEntity<List<PortfolioCareerResponse>> findCareer(@PathVariable(name = "id") String userId) throws Exception{
+    public ResponseEntity<List<PortfolioCareerResponse>> findCareer(@PathVariable(name = "id") String userId) throws RuntimeException{
         return new ResponseEntity<>(portfolioService.findCareer(userId), HttpStatus.OK);
     }
 
     @PostMapping(value = "/career/{id}")
-    public ResponseEntity<Boolean> addCareer(@PathVariable(name = "id") String userId, @RequestBody PortfolioCareerRequest request) throws Exception{
+    public ResponseEntity<Boolean> addCareer(@PathVariable(name = "id") String userId, @RequestBody PortfolioCareerRequest request) throws RuntimeException{
+        if (request.getCareerExplanation() == null || request.getCareerExplanation().equals("")){
+            throw new NullValueException("careerExplanation값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(portfolioService.addCareer(userId, request), HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/career/{id}")
-    public ResponseEntity<Boolean> deleteCareer(@PathVariable(name = "id") String userId, @RequestBody PortfolioCareerRequest request) throws Exception{
+    public ResponseEntity<Boolean> deleteCareer(@PathVariable(name = "id") String userId, @RequestBody PortfolioCareerRequest request) throws RuntimeException{
+        if (request.getCareerExplanation() == null || request.getCareerExplanation().equals("")){
+            throw new NullValueException("careerExplanation값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getPortfolioCareerId() == null || request.getPortfolioCareerId().equals("")){
+            throw new NullValueException("portfolioCareerId값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(portfolioService.deleteCareer(userId, request), HttpStatus.OK);
     }
 
     @PutMapping(value = "/career/{id}")
-    public ResponseEntity<Boolean> updateCareer(@PathVariable(name = "id") String userId, @RequestBody PortfolioCareerRequest request) throws Exception{
+    public ResponseEntity<Boolean> updateCareer(@PathVariable(name = "id") String userId, @RequestBody PortfolioCareerRequest request) throws RuntimeException{
+        if (request.getCareerExplanation() == null || request.getCareerExplanation().equals("")){
+            throw new NullValueException("careerExplanation값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getPortfolioCareerId() == null || request.getPortfolioCareerId().equals("")){
+            throw new NullValueException("portfolioCareerId값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(portfolioService.updateCareer(userId, request), HttpStatus.OK);
     }
 
     @GetMapping(value = "/image/{name}")
-    public ResponseEntity<ImageDownloadResponse> download(@PathVariable(name = "name") String imageName) throws Exception{
+    public ResponseEntity<ImageDownloadResponse> download(@PathVariable(name = "name") String imageName) throws RuntimeException{
         return new ResponseEntity<>(portfolioService.download(imageName), HttpStatus.OK);
     }
 
--- src/main/java/bcsd/backend/project/pokku/controller/SignController.java
+++ src/main/java/bcsd/backend/project/pokku/controller/SignController.java
@@ -4,6 +4,8 @@ import bcsd.backend.project.pokku.dto.Sign.SignInRequest;
 import bcsd.backend.project.pokku.dto.Sign.SignInResponse;
 import bcsd.backend.project.pokku.dto.Sign.SignOutRequest;
 import bcsd.backend.project.pokku.dto.Sign.SignUpRequest;
+import bcsd.backend.project.pokku.exception.NullValueException.NullValueException;
+import bcsd.backend.project.pokku.exception.ResCode;
 import bcsd.backend.project.pokku.service.Sign.SignServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,17 +27,53 @@ public class SignController {
     }
 
     @PostMapping(value = "/in")
-    public ResponseEntity<SignInResponse> signin(@RequestBody SignInRequest request) throws Exception{
+    public ResponseEntity<SignInResponse> signin(@RequestBody SignInRequest request) throws RuntimeException{
+        if (request.getUserId() == null || request.getUserId().equals("")){
+            throw new NullValueException("userId값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getUserPassword() == null || request.getUserPassword().equals("")){
+            throw new NullValueException("userPassword값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(signService.login(request), HttpStatus.OK);
     }
 
     @PostMapping(value = "/out")
-    public ResponseEntity<Boolean> signin(@RequestBody SignOutRequest request) throws Exception{
+    public ResponseEntity<Boolean> signout(@RequestBody SignOutRequest request) throws RuntimeException{
+        if (request.getUserId() == null || request.getUserId().equals("")){
+            throw new NullValueException("userId값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getToken() == null || request.getToken().equals("")){
+            throw new NullValueException("token값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(signService.logout(request), HttpStatus.OK);
     }
 
     @PostMapping(value = "/up")
-    public ResponseEntity<Boolean> signin(@RequestBody SignUpRequest request) throws Exception{
+    public ResponseEntity<Boolean> signup(@RequestBody SignUpRequest request) throws RuntimeException{
+        if (request.getUserName() == null || request.getUserName().equals("")){
+            throw new NullValueException("userName값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getUserPassword() == null || request.getUserPassword().equals("")){
+            throw new NullValueException("userPassword값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getUserId() == null || request.getUserId().equals("")){
+            throw new NullValueException("userId값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getUserTel() == null || request.getUserTel().equals("")){
+            throw new NullValueException("userTel값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getUserNickname() == null || request.getUserNickname().equals("")){
+            throw new NullValueException("userNickname값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getUserEmail() == null || request.getUserEmail().equals("")){
+            throw new NullValueException("userEmail값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getUserBirth() == null || request.getUserBirth().equals("")){
+            throw new NullValueException("userBirth값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
+        if (request.getUserEducation() == null || request.getUserEducation().equals("")){
+            throw new NullValueException("userEducation값이 비어있습니다.", null, ResCode.NULL_VALUE.value());
+        }
         return new ResponseEntity<>(signService.register(request), HttpStatus.OK);
     }
 }
