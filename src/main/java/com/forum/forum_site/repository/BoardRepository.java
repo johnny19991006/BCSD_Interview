0936a408186bca08f6b24aeae23d0bcf60b62600
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-02-03 17:16:47 +0900

    delete about board

--- src/main/java/com/forum/forum_site/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.forum.forum_site.repository;
-
-import com.forum.forum_site.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Integer> {
-}
