2bd49815f58c1a9753a560384ce82451f7f3b698
duehee <149302959+duehee@users.noreply.github.com>
2024-02-04 16:25:36 +0900

    chore : StudentGradeInformation 내 DB 수정

--- src/main/java/AcademicManagement/BCSDproject/Domain/StudentGradeInformation.java
+++ src/main/java/AcademicManagement/BCSDproject/Domain/StudentGradeInformation.java
@@ -1,5 +1,6 @@
 package AcademicManagement.BCSDproject.Domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +11,6 @@ import lombok.Setter;
 Create Table Student_Grade_Information
 (
 	student_id VARCHAR(20) PRIMARY KEY,
-    semester_id INT,
     total_credit INT NOT NULL,
     total_major_credit INT NOT NULL,
     avg_major_score FLOAT NOT NULL,
@@ -19,7 +19,6 @@ Create Table Student_Grade_Information
     total_all_credit INT NOT NULL,
     avg_all_score FLOAT NOT NULL,
     FOREIGN KEY (student_id) references Student(student_id),
-    FOREIGN KEY (semester_id) references Semester(semester_id)
 );
 */
 
@@ -32,9 +31,6 @@ public class StudentGradeInformation {
     @Id
     @Column(name = "student_id")
     private String studentId;
-
-    @Column(name = "semester_id")
-    private int semesterId;
 
     @Column(name = "total_credit", nullable = false)
     private int totalCredit;
@@ -59,19 +55,15 @@ public class StudentGradeInformation {
 
     @ManyToOne
     @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    @JsonIgnore
     private Student student;
 
-    @ManyToOne
-    @JoinColumn(name = "semester_id", insertable = false, updatable = false)
-    private Semester semester;
-
     @Builder
-    public StudentGradeInformation(String studentId, int semesterId, int totalCredit, int totalMajorCredit,
+    public StudentGradeInformation(String studentId, int totalCredit, int totalMajorCredit,
                                    float avgMajorScore, int totalGeneralCredit, float avgGeneralScore,
                                    int totalAllCredit, float avgAllScore)
     {
         this.studentId = studentId;
-        this.semesterId = semesterId;
         this.totalCredit = totalCredit;
         this.totalMajorCredit = totalMajorCredit;
         this.avgMajorScore = avgMajorScore;
